376799f3b7931b099e3c699d1f53688f924f99b0
Carolina Ferreira <112209581+carolmic@users.noreply.github.com>
2024-09-05 08:46:12 -0300

    Add files via upload

---
+++ ex1-biblioteca-v6-solServicoEstatisticas/src/main/java/com/bcopstein/ex1biblioeca/Ex1BiblioecaApplication.java
@@ -0,0 +1,16 @@
+package com.bcopstein.ex1biblioeca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// To do: criar o servico
+
+@SpringBootApplication
+public class Ex1BiblioecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex1BiblioecaApplication.class, args);
+	}
+
+}
+
